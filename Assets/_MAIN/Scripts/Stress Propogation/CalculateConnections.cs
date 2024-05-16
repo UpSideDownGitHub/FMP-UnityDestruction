b9c063bb8f3b401977d89236f7cd1a4c038a228c
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2024-05-16 03:16:49 +0100

    Removal Of Data

--- Assets/_MAIN/Scripts/Stress Propogation/CalculateConnections.cs
+++ Assets/_MAIN/Scripts/Stress Propogation/CalculateConnections.cs
@@ -15,6 +15,7 @@ namespace UnityFracture
         /// <summary>
         /// data structure to hold a basic line so the can be drawn to screen
         /// </summary>
+        [Serializable]
         public struct Line
         {
             public Line(Vector3 p1, Vector3 p2)
@@ -25,7 +26,7 @@ namespace UnityFracture
             public Vector3 p1;
             public Vector3 p2;
         }
-        private List<Line> lines = new();
+        public List<Line> lines = new();
 
         /// <summary>
         /// Draws the connections as line gizmos
@@ -44,19 +45,20 @@ namespace UnityFracture
         public void calculateConnections()
         {
             // remove all of the previous connections between the children
-            var childMeshes = gameObject.GetComponentsInChildren<MeshCollider>();
-            foreach (var collider in childMeshes)
+            var childMeshes = gameObject.GetComponentsInChildren<MeshFilter>();
+            var childColliders = gameObject.GetComponentsInChildren<Collider>();
+            foreach (var mesh in childMeshes)
             {
-                collider.gameObject.GetComponent<Connections>().RemoveAllConnections();
+                mesh.gameObject.GetComponent<Connections>().RemoveAllConnections();
             }
 
             // looping through all of the children calculate the connections between them
-            foreach (var collider in childMeshes)
+            foreach (var mesh in childMeshes)
             {
                 // get the triangle & verts of the current child
-                int triangleCount = collider.sharedMesh.triangles.Length;
-                var verts = collider.sharedMesh.vertices;
-                var tris = collider.sharedMesh.triangles;
+                int triangleCount = mesh.sharedMesh.triangles.Length;
+                var verts = mesh.sharedMesh.vertices;
+                var tris = mesh.sharedMesh.triangles;
 
                 // looping through all of the found trinagles
                 for (int i = 0; i < triangleCount; i += 3)
@@ -68,7 +70,7 @@ namespace UnityFracture
                     points[2] = verts[tris[i + 2]];
                     
                     // move the points to there world position
-                    collider.transform.TransformPoints(points);
+                    mesh.transform.TransformPoints(points);
 
                     // calculate the center of the triangle
                     var center = new Vector3((points[0].x + points[1].x + points[2].x) / 3,
@@ -76,28 +78,28 @@ namespace UnityFracture
                         (points[0].z + points[1].z + points[2].z) / 3);
 
                     // looping through all of the childre again
-                    foreach (var collider2 in childMeshes)
+                    foreach (var collider in childColliders)
                     {
-                        // if is the same collider then skip
-                        if (collider2 == collider)
+                        // if is the same mesh then skip
+                        if (collider.gameObject == mesh.gameObject)
                             continue;
 
-                        // get the closest point on the collider from the center of the given cirlce
-                        var point = collider2.ClosestPoint(center);
+                        // get the closest point on the mesh from the center of the given cirlce
+                        var point = collider.ClosestPoint(center);
 
                         // if the distance is close to 0 (touching)
                         if (Vector3.Distance(point, center) < epsillon)
                         {
                             // if this connection does not already exist then add a new connection between the two object
                             // as well as adding the line the list of lines for debugging.
-                            if (!collider.gameObject.GetComponent<Connections>().HasConnection(collider2.gameObject.GetComponent<Connections>()))
+                            if (!mesh.gameObject.GetComponent<Connections>().HasConnection(collider.gameObject.GetComponent<Connections>()))
                             {
                                 lines.Add(new Line(point, center ));
-                                collider.gameObject.GetComponent<Connections>().AddConnection(collider2.gameObject.GetComponent<Connections>()); 
+                                mesh.gameObject.GetComponent<Connections>().AddConnection(collider.gameObject.GetComponent<Connections>()); 
                             }
-                            if (!collider2.gameObject.GetComponent<Connections>().HasConnection(collider.gameObject.GetComponent<Connections>()))
+                            if (!collider.gameObject.GetComponent<Connections>().HasConnection(mesh.gameObject.GetComponent<Connections>()))
                             {
-                                collider2.gameObject.GetComponent<Connections>().AddConnection(collider.gameObject.GetComponent<Connections>());
+                                collider.gameObject.GetComponent<Connections>().AddConnection(mesh.gameObject.GetComponent<Connections>());
                             }
                         }
                     }
